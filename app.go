ff193c75c2e2a40e0a69da5e4ecd0963c8e911c3
EnakNalla <121622489+EnakNalla@users.noreply.github.com>
2022-12-29 16:47:31 +0000

    add file monitor logic
    using github.com/fsnotify/fsnotify

--- app.go
+++ app.go
@@ -1,14 +1,34 @@
 package main
 
 import (
+	"bufio"
 	"context"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/fsnotify/fsnotify"
+	"github.com/wailsapp/wails/v2/pkg/runtime"
 )
+
+type LogFile struct {
+	Path  string   `json:"path"`
+	Lines []string `json:"lines"`
+}
+
+type Result struct {
+	Success bool    `json:"success"`
+	Data    LogFile `json:"data"`
+	Error   string  `json:"error"`
+}
 
 type App struct {
 	ctx context.Context
 }
 
-// NewApp creates a new App application struct
+var watcher, err = fsnotify.NewWatcher()
+var logs = map[string]LogFile{}
+
 func NewApp() *App {
 	return &App{}
 }
@@ -16,4 +36,144 @@ func NewApp() *App {
 // startup is called when the app starts. The context is saved so we can call the runtime methods
 func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
+
+	if err != nil {
+		panic(err)
+	}
+	defer watcher.Close()
+
+	go func() {
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					file, exists := logs[event.Name]
+					if exists {
+						line, err := readLastLine(file.Path)
+						if err != nil {
+							runtime.LogErrorf(ctx, err.Error())
+						} else {
+							runtime.EventsEmit(ctx, file.Path, line)
+						}
+					}
+				}
+			}
+		}
+	}()
+
+	<-make(chan struct{})
+}
+
+func (a *App) SelectFile() Result {
+	selection, err := runtime.OpenFileDialog(a.ctx, runtime.OpenDialogOptions{
+		Title: "Select log",
+		Filters: []runtime.FileFilter{
+			{
+				DisplayName: "Logs (*.txt;*.log)",
+				Pattern:     "*.txt;*.log",
+			},
+		},
+	})
+	if err != nil || selection == "" {
+		if err != nil {
+			runtime.LogErrorf(a.ctx, err.Error())
+		} else {
+			runtime.LogErrorf(a.ctx, "No file selection made")
+		}
+		return Result{Data: LogFile{}, Success: false, Error: "Failed to open file."}
+	}
+
+	_, exists := logs[selection]
+	if exists {
+		return Result{Data: LogFile{}, Success: false, Error: "File with path exists."}
+	}
+
+	logs[selection] = LogFile{Path: selection}
+
+	return addWatcher(selection, a)
+}
+
+func addWatcher(path string, a *App) Result {
+	logs[path] = LogFile{Path: path}
+
+	err = watcher.Add(path)
+	if err != nil {
+		delete(logs, path)
+
+		runtime.LogErrorf(a.ctx, err.Error())
+		return Result{Data: LogFile{}, Success: false, Error: err.Error()}
+	}
+
+	result, err := readLines(logs[path])
+	if err != nil {
+		runtime.LogErrorf(a.ctx, err.Error())
+	}
+
+	return result
+}
+
+func readLines(logFile LogFile) (Result, error) {
+	file, err := os.Open(logFile.Path)
+	if err != nil {
+		delete(logs, logFile.Path)
+
+		return Result{Data: logFile, Success: false, Error: "Failed to read file"}, err
+	}
+	defer file.Close()
+
+	fileScanner := bufio.NewScanner(file)
+	fileScanner.Split(bufio.ScanLines)
+
+	for fileScanner.Scan() {
+		logFile.Lines = append(logFile.Lines, fileScanner.Text())
+	}
+
+	return Result{Data: logFile, Success: true, Error: ""}, nil
+}
+
+func readLastLine(file string) (string, error) {
+	fileHandle, err := os.Open(file)
+	if err != nil {
+		return "", err
+	}
+	defer fileHandle.Close()
+
+	line := ""
+	var cursor int64 = 0
+	stat, _ := fileHandle.Stat()
+	filesize := stat.Size()
+	for {
+		cursor -= 1
+		fileHandle.Seek(cursor, io.SeekEnd)
+
+		char := make([]byte, 1)
+		fileHandle.Read(char)
+
+		if cursor != -1 && (char[0] == 10 || char[0] == 13) {
+			break
+		}
+
+		line = fmt.Sprintf("%s%s", string(char), line)
+
+		if cursor == -filesize {
+			break
+		}
+	}
+
+	return line, nil
+}
+
+func (a *App) RemoveWatcher(path string) Result {
+	delete(logs, path)
+
+	err = watcher.Remove(path)
+	if err != nil {
+		runtime.LogErrorf(a.ctx, err.Error())
+		return Result{Data: LogFile{}, Success: false, Error: err.Error()}
+	}
+
+	return Result{Data: LogFile{}, Success: true, Error: ""}
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require github.com/wailsapp/wails/v2 v2.2.0
 
 require (
 	github.com/bep/debounce v1.2.1 // indirect
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/google/uuid v1.1.2 // indirect
 	github.com/imdario/mergo v0.3.12 // indirect
@@ -27,7 +28,7 @@ require (
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
 	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
 	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
-	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
+	golang.org/x/sys v0.0.0-20220908164124-27713097b956 // indirect
 	golang.org/x/text v0.3.7 // indirect
 )
 
